25a2c15a15b68765f247a56243d235e833a9c5a9
plungerman <plungerman@gmail.com>
2016-07-02 16:01:39 -0500

    updated default settings from current settings file

--- djpsilobus/settings_default.py
+++ djpsilobus/settings_default.py
@@ -1,23 +1,23 @@
 """
 Django settings for project.
 """
+#from djzbar.settings import INFORMIX_EARL_TEST as INFORMIX_EARL
+from djzbar.settings import INFORMIX_EARL_PROD as INFORMIX_EARL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 # Debug
-#DEBUG = False
 DEBUG = True
+#DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 INFORMIX_DEBUG = "debug"
 ADMINS = (
-    ('', ''),
+    ('admin', 'admin@example.com'),
 )
 MANAGERS = ADMINS
-
-SECRET_KEY = ''
-ALLOWED_HOSTS = []
-
+SECRET_KEY = ""
+ALLOWED_HOSTS =  ['localhost','127.0.0.1']
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/Chicago'
 SITE_ID = 1
@@ -26,30 +26,33 @@ USE_L10N = False
 USE_TZ = False
 DEFAULT_CHARSET = 'utf-8'
 FILE_CHARSET = 'utf-8'
-SERVER_URL = ""
-API_URL = "%s/%s" % (SERVER_URL, "api")
-LIVEWHALE_API_URL = "https://%s" % (SERVER_URL)
+SERVER_URL = "www.example.com"
+API_URL = "{}/{}".format(SERVER_URL, "api")
+LIVEWHALE_API_URL = "https://{}".format(SERVER_URL)
+ROOT_URL = "/djpsilobus/"
+ROOT_URLCONF = 'djpsilobus.urls'
+WSGI_APPLICATION = 'djpsilobus.wsgi.application'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 ROOT_DIR = os.path.dirname(__file__)
-ROOT_URL = "/djskeletor/"
-ROOT_URLCONF = 'djskeletor.core.urls'
-WSGI_APPLICATION = 'djskeletor.wsgi.application'
-MEDIA_ROOT = ''
 ADMIN_MEDIA_PREFIX = '/static/admin/'
+MEDIA_ROOT = '{}/assets/'.format(ROOT_DIR)
+#MEDIA_ROOT = ''
+#STATIC_ROOT = '{}/static/'.format(ROOT_DIR)
 STATIC_ROOT = ''
-STATIC_URL = "/static/"
+STATIC_URL = "/static/djpsilobus/"
+MEDIA_URL = '{}assets/'.format(STATIC_URL)
+UPLOADS_DIR = "{}files/".format(MEDIA_ROOT)
+UPLOADS_URL = "{}files/".format(MEDIA_URL)
 STATICFILES_DIRS = ()
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
 )
-
 DATABASES = {
     'default': {
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'NAME': 'django_djskeletor',
+        'NAME': 'django_djsani',
         'ENGINE': 'django.db.backends.mysql',
         #'ENGINE': 'django.db.backends.dummy',
         'USER': '',
@@ -66,17 +69,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'djskeletor',
-    'djskeletor.core',
-    'djskeletor.myapp',
+    'djpsilobus',
+    'djpsilobus.core',
     # needed for template tags
     'djtools',
 )
-
 MIDDLEWARE_CLASSES = (
-    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,6 +92,7 @@ TEMPLATE_LOADERS = (
 )
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
+    "/data2/django_projects/djpsilobus/templates/",
     "/data2/django_templates/djkorra/",
     "/data2/django_templates/djcher/",
     "/data2/django_templates/",
@@ -107,49 +107,46 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # caching
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        #'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        #'LOCATION': '127.0.0.1:11211',
-        #'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        #'LOCATION': '/var/tmp/django_djskeletor_cache',
-        #'TIMEOUT': 60*20,
-        #'KEY_PREFIX': "DJSKELETOR_",
-        #'OPTIONS': {
-        #    'MAX_ENTRIES': 80000,
-        #}
+        #'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        #'TIMEOUT': None, # never expire
+        'TIMEOUT': 60*60*24,
+        'KEY_PREFIX': "DJPSILO_",
+        'OPTIONS': {
+            'MAX_ENTRIES': 500000,
+            'CULL_FREQUENCY':20 # cull 5% (1/20) of objects when max_entries
+        }
     }
 }
-CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
 # LDAP Constants
 LDAP_SERVER = ''
-LDAP_SERVER_PWM = ''
 LDAP_PORT = ''
-LDAP_PORT_PWM = ''
-LDAP_PROTOCOL = ""
-LDAP_PROTOCOL_PWM = ""
-LDAP_BASE = ""
-LDAP_USER = ""
-LDAP_PASS = ""
-LDAP_EMAIL_DOMAIN = ""
-LDAP_OBJECT_CLASS = ""
+LDAP_PROTOCOL = ''
+LDAP_BASE = ''
+LDAP_USER = ''
+LDAP_PASS = ''
+LDAP_EMAIL_DOMAIN = ''
+LDAP_OBJECT_CLASS = ''
 LDAP_OBJECT_CLASS_LIST = []
 LDAP_GROUPS = {}
 LDAP_RETURN = []
-LDAP_RETURN_PWM = []
-LDAP_ID_ATTR = ""
-LDAP_CHALLENGE_ATTR = ""
+LDAP_ID_ATTR=''
+LDAP_AUTH_USER_PK = False
 # auth backends
 AUTHENTICATION_BACKENDS = (
     'djauth.ldapBackend.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
-LOGIN_URL = '/djskeletor/accounts/login/'
-LOGIN_REDIRECT_URL = '/djskeletor/'
+LOGIN_URL = '{}accounts/login/'.format(ROOT_URL)
+LOGOUT_URL = '{}accounts/logout/'.format(ROOT_URL)
+LOGIN_REDIRECT_URL = ROOT_URL
 USE_X_FORWARDED_HOST = True
 #SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_COOKIE_DOMAIN=".carthage.edu"
-SESSION_COOKIE_NAME ='django_djskeletor_cookie'
+SESSION_COOKIE_DOMAIN=''
+SESSION_COOKIE_NAME ='django_djpsilobus_cookie'
 SESSION_COOKIE_AGE = 86400
 # SMTP settings
 EMAIL_HOST = ''
@@ -157,10 +154,23 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_FAIL_SILENTLY = False
+EMAIL_FAIL_SILENTLY = True
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = ''
 SERVER_MAIL=''
+# dSpace constants
+DSPACE_EMAIL = ""
+DSPACE_PASSWORD = ""
+DSPACE_TOKEN = ""
+DSPACE_URL = "https://dspace.example.com"
+DSPACE_REST_URL = "{}/rest".format(DSPACE_URL)
+# Registrar Administrators
+REGISTRAR_ADMIN = []
+# Administrative Assistants
+ADMIN_ASSISTANTS = []
+# developers
+DEV_ADMIN = []
+ADMINISTRATORS = REGISTRAR_ADMIN + ADMIN_ASSISTANTS + DEV_ADMIN
 # logging
 LOG_FILEPATH = os.path.join(os.path.dirname(__file__), "logs/")
 LOG_FILENAME = LOG_FILEPATH + "debug.log"
@@ -186,10 +196,6 @@ LOGGING = {
         }
     },
     'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
         'logfile': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
@@ -211,7 +217,7 @@ LOGGING = {
         }
     },
     'loggers': {
-        'djskeletor': {
+        'djpsilobus.core': {
             'handlers':['logfile'],
             'propagate': True,
             'level':'DEBUG',
