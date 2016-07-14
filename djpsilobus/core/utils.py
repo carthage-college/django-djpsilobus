2423c5bbf8d29144cb26ab251ffda71e5fa5790c
plungerman <plungerman@gmail.com>
2016-07-14 14:38:40 -0500

    new function for creating an item in dspace

--- djpsilobus/core/utils.py
+++ djpsilobus/core/utils.py
@@ -1,9 +1,16 @@
 from django.conf import settings
 
+from djpsilobus.core.data import DEPARTMENTS, ITEM_METADATA
 from djpsilobus.core.dspace import Manager
+
+from djzbar.utils.informix import get_session
+from djzbar.core.models.courses import AbstractRecord
+from djzbar.constants import TERM_LIST
 
 import json
 import requests
+
+EARL = settings.INFORMIX_EARL
 
 
 def find_file(phile):
@@ -28,3 +35,50 @@ def find_file(phile):
     )
 
     return jason
+
+def create_item(item):
+    """
+    Accepts a dictionary with the following keys:
+    course_number, title, year, term, user
+    """
+
+    data = ITEM_METADATA
+
+    # create database session
+    session = get_session(EARL)
+
+    cat = "UG{}".format(item["year"][-2:])
+    c = session.query(AbstractRecord).\
+        filter_by(crs_no = item["course_number"]).\
+        filter_by(cat = cat).one()
+
+    abstr = c.abstr.split('\n')
+    if len(abstr) > 1 and abstr[2] != "":
+        abstr = abstr[2]
+    else:
+        abstr = c.abstr
+
+    dept = item["course_number"].split(" ")[0]
+    collection_id = DEPARTMENTS[dept]
+    # author
+    data['metadata'][0]['value'] = "{}, {}".format(
+        item["user"].last_name, item["user"].first_name
+    )
+    # description
+    data['metadata'][1]['value'] = c.abstr.split('\n')[2]
+    # title
+    data['metadata'][2]['value'] = item["title"]
+    # subject: year
+    data['metadata'][3]['value'] = item["year"]
+    # subject: term
+    data['metadata'][4]['value'] = TERM_LIST[item["term"]]
+    uri = "collections/{}/items".format(collection_id)
+
+    print data
+    print uri
+
+    manager = Manager()
+    new_item = manager.request(data, uri, "post")
+
+    return new_item
+
