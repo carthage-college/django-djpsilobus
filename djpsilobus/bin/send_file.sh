dc50c8b9bda4218a9ae4693655b663337fc3eed5
plungerman <plungerman@gmail.com>
2016-09-19 11:23:46 -0500

    removed 'response' dictionary

--- djpsilobus/bin/send_file.sh
+++ djpsilobus/bin/send_file.sh
@@ -3,7 +3,7 @@
 DSPACEURL="https://dspace.carthage.edu/rest"
 FILE="/data2/www/Rosa_Luxemburg_Huelga_masas_23.pdf"
 VERB="POST"
-TOKEN="d8f00d42-e75a-4744-b54f-b70a2922c17f"
+TOKEN=""
 ACTION="items/1279/bitstreams/?name=Rosa_Luxemburg_Huelga_masas_23.pdf"
 EARL="${DSPACEURL}/${ACTION}"
 echo ${EARL}
@@ -18,5 +18,3 @@ curl -i -k -4 \
     -X ${VERB} ${EARL} \
     -F upload=@"${FILE}"
 
-# response
-#{"id":4187,"name":"Resistencia_ENERO_MARZO_baja.pdf","handle":null,"type":"bitstream","link":"/RESTapi/bitstreams/4187","expand":["parent","policies","all"],"bundleName":"ORIGINAL","description":null,"format":"Adobe PDF","mimeType":"application/pdf","sizeBytes":551479,"parentObject":null,"retrieveLink":"/bitstreams/4187/retrieve","checkSum":{"value":"9670d137be9a5810f42e40aabef1f0d6","checkSumAlgorithm":"MD5"},"sequenceId":-1,"policies":null}
