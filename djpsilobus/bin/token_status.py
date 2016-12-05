bd0de09d9da908a0028372a0dc54a099de3739ba
plungerman <plungerman@gmail.com>
2016-12-05 14:30:23 -0600

    check token status and cache for a working token to speed up response times

--- djpsilobus/bin/token_status.py
+++ djpsilobus/bin/token_status.py
@@ -3,6 +3,14 @@ from django.conf import settings
 import json
 import requests
 
+'''
+returns a json object:
+{
+    "okay":true,"authenticated":true,"email":"larry@example.com",
+    "fullname":"Larry","token":"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+'''
+
 tipe = 'json'
 action = 'status'
 url = 'https://dspace.carthage.edu/rest/{}'.format(action)
@@ -17,6 +25,8 @@ headers = {
 response = requests.get(url, headers=headers)
 #print response.__dict__
 print "token status:\n\n"
-print response._content
-
+#print response._content["authenticated"]
+r = json.loads( response._content )
+print r
+print r["authenticated"]
 
--- djpsilobus/core/dspace.py
+++ djpsilobus/core/dspace.py
@@ -1,25 +1,58 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.core.cache import cache
 
 import json
 import requests
+
+REST_URL = settings.DSPACE_REST_URL
+def _get_token():
+    # obtain our token from DSpace server
+    login_url = "{}/login".format(REST_URL)
+    login_dict = {
+        "email":"{}".format(settings.DSPACE_EMAIL),
+        "password":"{}".format(settings.DSPACE_PASSWORD)
+    }
+    token = requests.post(
+        login_url, data=json.dumps(login_dict),
+        headers={'content-type': 'application/json'}
+    )
+    return token._content
 
 class Manager(object):
 
     def __init__(self):
 
-        self.rest_url = settings.DSPACE_REST_URL
-        # obtain our token from DSpace server
-        login_url = "{}/login".format(self.rest_url)
-        login_dict = {
-            "email":"{}".format(settings.DSPACE_EMAIL),
-            "password":"{}".format(settings.DSPACE_PASSWORD)
-        }
-        token = requests.post(
-            login_url, data=json.dumps(login_dict),
-            headers={'content-type': 'application/json'}
-        )
-        token = token._content
+        token = cache.get("DSPACE_API_TOKEN")
+        if not token:
+            # obtain our token from DSpace server
+            login_url = "{}/login".format(REST_URL)
+            login_dict = {
+                "email":"{}".format(settings.DSPACE_EMAIL),
+                "password":"{}".format(settings.DSPACE_PASSWORD)
+            }
+            token = requests.post(
+                login_url, data=json.dumps(login_dict),
+                headers={'content-type': 'application/json'}
+            )
+            token = token._content
+            token = _get_token()
+            cache.set("DSPACE_API_TOKEN", token, None)
+        else:
+            # check token status
+            headers = {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'rest-dspace-token': token
+            }
+            response = requests.get(
+                '{}/status'.format(REST_URL),
+                headers=headers
+            )
+            r = json.loads( response._content )
+
+            if r["authenticated"] != "true":
+                token = _get_token()
         self.headers = {
             "Content-Type": "application/json",
             "rest-dspace-token": "{}".format(token),
@@ -31,7 +64,7 @@ class Manager(object):
         if not headers:
             headers = self.headers
 
-        earl = "{}/{}".format(self.rest_url, uri)
+        earl = "{}/{}".format(REST_URL, uri)
         if action == "post":
             action = requests.post
         elif action == "get":
