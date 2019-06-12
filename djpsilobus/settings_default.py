e30fe934c8095857d708d044e2ba98db901322a1
plungerman <plungerman@gmail.com>
2019-06-12 18:16:02 -0500

    moved URL connection string from djzbar to django project's settings file for python 3.6 migration

--- djpsilobus/settings_default.py
+++ djpsilobus/settings_default.py
@@ -3,6 +3,8 @@ Django settings for project.
 """
 #from djzbar.settings import INFORMIX_EARL_TEST as INFORMIX_EARL
 from djzbar.settings import INFORMIX_EARL_PROD as INFORMIX_EARL
+# sqlserver connection string
+MSSQL_EARL = ''
 from djtools.fields import TODAY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
