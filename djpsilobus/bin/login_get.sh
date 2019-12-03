bf54de94bfc5ea8994b00183a689880456b2dc86
plungerman <plungerman@gmail.com>
2019-12-03 09:15:04 -0600

    shell scripts updates for testing new authentication for dspace 6.x

--- djpsilobus/bin/login_get.sh
+++ djpsilobus/bin/login_get.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+# set the EMAIL and PASSWORD env variables from command line
+# export PASSWORD="xxx"
+# export EMAIL="spam@example.com"
+
+echo ${EMAIL}
+echo ${PASSWORD}
 
 DSPACEURL="https://carthage.upgrade.dspace-express.com/rest"
-EMAIL="larry@carthage.edu"
-PASSWORD=""
 ACTION="login?"
 RQST="email=${EMAIL}&password=${PASSWORD}"
 URL="${DSPACEURL}/${ACTION}${RQST}"
@@ -10,4 +15,6 @@ URL="${DSPACEURL}/${ACTION}${RQST}"
 echo ${DSPACEURL}
 echo ${URL}
 
-curl -D - "${URL}"
+#curl -D - "${URL}"
+curl -D - --data-urlencode "password=$PASSWORD" --data-urlencode "email=$EMAIL" "${DSPACEURL}/${ACTION}"
+
--- djpsilobus/bin/login_post.sh
+++ djpsilobus/bin/login_post.sh
@@ -2,7 +2,8 @@
 
 DSPACEURL="https://dspace.carthage.edu/rest"
 EMAIL="larry@carthage.edu"
-PASSWORD=""
+#PASSWORD=""
+PASSWORD="wOr7I#v*6cdIBcuQ%Q8EkDux@&FV40Ln"
 TYPE="json"
 ACTION="login"
 
@@ -16,7 +17,9 @@ RQST="{
 echo ${DSPACEURL}
 echo ${URL}
 
-curl -4 \
-  -H "Content-Type: application/${TYPE}" \
-  --data "${RQST}" \
-  "${URL}"
+#curl -4 \
+  #-H "Content-Type: application/${TYPE}" \
+  #--data "${RQST}" \
+  #"${URL}"
+
+curl -v -X POST --data "email=${EMAIL}&password=${PASSWORD}" ${URL}
